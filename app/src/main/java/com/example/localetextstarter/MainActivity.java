f467329e158de1f69e34ababf92c6a617ac05927
fachri <you@example.com>
2023-04-03 21:57:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/localetextstarter/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.localetextstarter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etPrice;
+    TextView tvResult;
+    Button btnCount;
+
+    float result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showHelp();
+            }
+        });
+
+        Date myDate = new Date();
+        long expirationDate = myDate.getTime() + TimeUnit.DAYS.toMillis(3);
+        myDate.setTime(expirationDate);
+
+        String formatDate = DateFormat.getDateInstance().format(myDate);
+        TextView expiredTextView = findViewById(R.id.date);
+        expiredTextView.setText(formatDate);
+
+        etPrice = findViewById(R.id.etPrice);
+        tvResult = findViewById(R.id.tvResult);
+        btnCount = findViewById(R.id.bCount);
+
+        btnCount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String value = etPrice.getText().toString();
+                result = (float) Float.parseFloat(value);
+
+                float r2 = result * 100;
+
+                NumberFormat format = NumberFormat.getCurrencyInstance();
+                format.setMaximumFractionDigits(0);
+                format.setCurrency(Currency.getInstance(Locale.getDefault()));
+
+                String r3 = format.format(r2);
+                tvResult.setText(r3);
+            }
+        });
+    }
+
+    /**
+     * Shows the Help screen.
+     */
+    private void showHelp() {
+        // Create the intent.
+        Intent helpIntent = new Intent(this, HelpActivity.class);
+        // Start the HelpActivity.
+        startActivity(helpIntent);
+    }
+
+    /**
+     * Creates the options menu and returns true.
+     *
+     * @param menu       Options menu
+     * @return boolean   True after creating options menu.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    /**
+     * Handles options menu item clicks.
+     *
+     * @param item      Menu item
+     * @return boolean  True if menu item is selected.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle options menu item clicks here.
+        int id = item.getItemId();
+        if (id == R.id.action_help) {
+            Intent intent = new Intent(this, HelpActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.action_language) {
+            Intent languageIntent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(languageIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
